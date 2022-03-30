da0afe4b6a600fa2b976972a3b4037810f931e61
Dave Ross <winglessravens@gmail.com>
2022-03-30 20:52:08 +0100

    * Fix issue #25 * Add colour customisations to embeds

--- DiscordBotPlugin/Settings.cs
+++ DiscordBotPlugin/Settings.cs
@@ -9,93 +9,131 @@ namespace DiscordBotPlugin
         public class DiscordBotSettings : SettingSectionStore
         {
             [StoreEncrypted]
-            [WebSetting("Discord Bot Token", "(from [Discord Developer Site](https://discord.com/developers/applications))", false, "", CustomFieldTypes.Password)]
+            [WebSetting("Discord Bot Token", "From the [Discord Developer Site](https://discord.com/developers/applications)", false, "", CustomFieldTypes.Password)]
             public string BotToken = "";
 
-            [WebSetting("Bot Tagline", "(displayed at the bottom of bot embeds)", false)]
-            public string BotTagline = "Powered by Wingless Ravens";
+            [WebSetting("Bot Tagline", "Displayed at the bottom of bot embeds", false)]
+            public string BotTagline = "Powered by AMP";
 
-            [WebSetting("Server Display Name", "(displayed in @bot info response)", false)]
+            [WebSetting("Server Display Name", "Displayed in the bot info panel", false)]
             public string ServerDisplayName = "";
 
-            [WebSetting("Server Connection URL", "(displayed in @bot info response)", false)]
+            [WebSetting("Server Connection URL", "Displayed in bot info panel", false)]
             public string ServerConnectionURL = "";
 
-            [WebSetting("Server Password", "(displayed in @bot info response)", false)]
+            [WebSetting("Server Password", "Displayed in bot info panel", false)]
             public string ServerPassword = "";
 
-            [WebSetting("Modpack URL", "(displayed in @bot info response)", false)]
+            [WebSetting("Modpack URL", "Displayed in bot info panel", false)]
             public string ModpackURL = "";
 
-            [WebSetting("Game Image URL", "(displayed in @bot info response, needs to be a publicly accessible image URL)", false)]
+            [WebSetting("Game Image URL", "Displayed in bot info panel, needs to be a publicly accessible image URL", false)]
             public string GameImageURL = "";
 
-            [WebSetting("Valid Player Count?", "(if the player count reports correctly, for info and bot status)", false)]
+            [WebSetting("Valid Player Count?", "If the player count reports correctly, for info panel and bot status", false)]
             public bool ValidPlayerCount = false;
 
-            [WebSetting("Bot Activated", "(turn the bot on and off)", false)]
+            [WebSetting("Bot Activated", "Turn the bot on and off", false)]
             public bool BotActive = false;
 
-            [WebSetting("Bot Refresh Interval", "(how often, in seconds, the bot should update the presence and info message.  Recommended minimum 30 seconds otherwise requests to update Discord could be throttled)", false)]
+            [WebSetting("Bot Refresh Interval", "How often, in seconds, the bot should update the presence and info panel.  Recommended minimum 30 seconds otherwise requests to update Discord could be throttled", false)]
             public int BotRefreshInterval = 30;
 
-            [WebSetting("Restrict Functions to Discord Role", "(restrict server functions (start/stop/restart/kill/update) to Discord Role)", false)]
+            [WebSetting("Restrict Functions to Discord Role", "Restrict server functions (start/stop/restart/kill/update/console/manage) to Discord role", false)]
             public bool RestrictFunctions = false;
 
-            [WebSetting("Discord Role Name", "(name of the role in your Discord server that should be allowed to excecute server starts/stops)", false)]
+            [WebSetting("Discord Role Name", "Name of the role in your Discord server that should be allowed to excecute server functions", false)]
             public string DiscordRole = "";
 
-            [WebSetting("Log Button Presses and Commands", "(log when buttons are pressed and commands are used)", false)]
+            [WebSetting("Log Button Presses and Commands", "Log when buttons are pressed and commands are used", false)]
             public bool LogButtonsAndCommands = false;
 
-            [WebSetting("Button/Command Log Channel Name", "(channel name of where to log button presses and commands. If left blank response will be the same channel as the info display)", false)]
+            [WebSetting("Button/Command Log Channel Name", "Channel name of where to log button presses and commands. If left blank response will be the same channel as the info panel display", false)]
             public string ButtonResponseChannel = "";
 
             public List<string> InfoMessageDetails = new List<string>();
 
-            [WebSetting("Post Join and Leave Messages", "(post player join and leave events to a Discord channel)", false)]
+            [WebSetting("Post Join and Leave Messages", "Post player join and leave events to a Discord channel", false)]
             public bool PostPlayerEvents = false;
 
-            [WebSetting("Post Join and Leave Messages Channel Name", "(channel name to post player join and leave events)", false)]
+            [WebSetting("Post Join and Leave Messages Channel Name", "Channel name to post player join and leave events", false)]
             public string PostPlayerEventsChannel = "";
 
-            [WebSetting("Base Management URL", "(address used to manage instances, should be in the format of amp.domain.com or your external IP if you do not use a domain)", false)]
+            [WebSetting("Base Management URL", "Address used to manage instances, should be in the format of amp.domain.com or your external IP if you do not use a domain", false)]
             public string ManagementURL = "";
 
-            [WebSetting("Display Start Button", "(toggle the start button visibility on the info message)", false)]
+            [WebSetting("Display Start Button", "Toggle the start button visibility on the info panel", false)]
             public bool ShowStartButton = false;
 
-            [WebSetting("Display Stop Button", "(toggle the stop button visibility on the info message)", false)]
+            [WebSetting("Display Stop Button", "Toggle the stop button visibility on the info panel", false)]
             public bool ShowStopButton = false;
 
-            [WebSetting("Display Restart Button", "(toggle the restart button visibility on the info message)", false)]
+            [WebSetting("Display Restart Button", "Toggle the restart button visibility on the info panel", false)]
             public bool ShowRestartButton = false;
 
-            [WebSetting("Display Kill Button", "(toggle the kill button visibility on the info message)", false)]
+            [WebSetting("Display Kill Button", "Toggle the kill button visibility on the info panel", false)]
             public bool ShowKillButton = false;
 
-            [WebSetting("Display Update Button", "(toggle the update button visibility on the info message)", false)]
+            [WebSetting("Display Update Button", "Toggle the update button visibility on the info panel", false)]
             public bool ShowUpdateButton = false;
 
-            [WebSetting("Display Manage Button", "(toggle the manage button visibility on the info message)", false)]
+            [WebSetting("Display Manage Button", "Toggle the manage button visibility on the info panel", false)]
             public bool ShowManageButton = false;
 
-            [WebSetting("Display Online Player List", "(display online player list in the info panel - if not compatible with the server nothing will show)", false)]
+            [WebSetting("Display Online Player List", "Display online player list in the info panel - if not compatible with the server nothing will show", false)]
             public bool ShowOnlinePlayers = false;
 
-            [WebSetting("Change Displayed Status", "(replace the default AMP status with your own - key is the AMP value, value is the replacement - possible values listed on the [Wiki](https://github.com/winglessraven/AMP-Discord-Bot/wiki/Changing-Application-State-Values-to-Custom-Text))", false)]
+            [WebSetting("Change Displayed Status", "Replace the default AMP status with your own - key is the AMP value, value is the replacement - possible values listed on the [Wiki](https://github.com/winglessraven/AMP-Discord-Bot/wiki/Changing-Application-State-Values-to-Custom-Text)", false)]
             public Dictionary<string, string> ChangeStatus = new Dictionary<string, string>();
 
-            [WebSetting("Online Server Bot Presence Text", "(change the presence text when application is running, use {OnlinePlayers} and {MaximumPlayers} as variables)", false)]
+            [WebSetting("Online Server Bot Presence Text", "Change the presence text when application is running, use {OnlinePlayers} and {MaximumPlayers} as variables", false)]
             public string OnlineBotPresence = "";
 
             public Dictionary<string, TimeSpan> PlayTime = new Dictionary<string, TimeSpan>();
 
-            [WebSetting("Display Playtime Leaderboard", "(display the playtime leaderboard on the info panel - top 5 are shown)", false)]
+            [WebSetting("Display Playtime Leaderboard", "Display the playtime leaderboard on the info panel - top 5 are shown", false)]
             public bool ShowPlaytimeLeaderboard = false;
 
         }
 
         public DiscordBotSettings MainSettings = new DiscordBotSettings();
+
+        public class DiscordBotColoursSettings : SettingSectionStore
+        {
+            [WebSetting("Info Panel Colour", "Colour of the info panel embed message. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string InfoPanelColour = "";
+
+            [WebSetting("Start Server Colour", "Colour of the embed message when starting server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerStartColour = "";
+
+            [WebSetting("Stop Server Colour", "Colour of the embed message when stopping server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerStopColour = "";
+
+            [WebSetting("Restart Server Colour", "Colour of the embed message when restarting server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerRestartColour = "";
+
+            [WebSetting("Kill Server Colour", "Colour of the embed message when killing server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerKillColour = "";
+
+            [WebSetting("Update Server Colour", "Colour of the embed message when updating server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerUpdateColour = "";
+
+            [WebSetting("Manage Link Colour", "Colour of the embed message when someone requests the manage link if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ManageLinkColour = "";
+
+            [WebSetting("Console Command Colour", "Colour of the embed message when a console message is sent to the server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ConsoleCommandColour = "";
+
+            [WebSetting("Player Join Event Colour", "Colour of the embed message when a player joins the server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerPlayerJoinEventColour = "";
+
+            [WebSetting("Player Leave Event Colour", "Colour of the embed message when a player leaves the server if logging is enabled. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string ServerPlayerLeaveEventColour = "";
+
+            [WebSetting("Playtime Leaderboard Colour", "Colour of the embed message for the playtime leaderboard. Get the HEX colour from [here](https://www.color-hex.com/color-wheel/)", false)]
+            public string PlaytimeLeaderboardColour = "";
+        }
+
+        public DiscordBotColoursSettings ColourSettings = new DiscordBotColoursSettings();
     }
 }
