6abd774f22c9fd732c4f36cad0e5109371104b40
Bluscream <Bluscream@users.noreply.github.com>
2024-02-16 14:24:21 +0100

    Use dict instead of list for instance_dirs so i dont have to call os.basename over and over again

--- install_script.py
+++ install_script.py
@@ -26,9 +26,11 @@ def get_user_input(prompt, default=None):
 
 base_path = 'C:\\AMPDatastore\\Instances\\'
 developer_license_key = ''
+
 github_api_url = 'https://api.github.com/repos/winglessraven/AMP-Discord-Bot/releases/latest'
 dll_file_name = 'DiscordBotPlugin.dll'
 dll_file_path = os.path.join(script_dir, dll_file_name)
+plugin_config_file_name = 'DiscordBotPlugin.kvp'
 
 def execute_os_command(command):
     process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -59,13 +61,28 @@ def update_amp_config(instance_path):
                 with open(config_path, 'w') as f:
                     f.writelines(lines)
                 logger.info(f'Updated AMP.LoadPlugins in AMPConfig.conf')
+            else:
+                logger.info(f'DiscordBotPlugin already in AMP.LoadPlugins')
             break
 
-def get_instance_dirs(base_path):
-    for subfolder in os.listdir(base_path):
-        path = os.path.join(base_path, subfolder)
-        if not os.path.isdir(path): continue
-        yield path
+def copy_dll_to_plugin_folder(instance_path):
+    discordbot_plugin_dir = os.path.join(instance_path, 'Plugins', 'DiscordBotPlugin')
+    discordbot_plugin_file = os.path.join(discordbot_plugin_dir, dll_file_name)
+    if not os.path.exists(discordbot_plugin_dir):
+        logger.info(f'Creating DiscordBotPlugin folder at {discordbot_plugin_dir}')
+        os.makedirs(discordbot_plugin_dir, exist_ok=True)
+    logger.info(f'Copying {dll_file_path} to {discordbot_plugin_dir}')
+    if os.path.exists(discordbot_plugin_file):
+        logger.info(f'{discordbot_plugin_file} already exists, overwriting')
+    shutil.copy2(dll_file_path, discordbot_plugin_dir)
+
+def get_instance_dirs(base_path) -> dict[str, str]:
+    ret = {}
+    for instance_folder in os.listdir(base_path):
+        instance_path = os.path.join(base_path, instance_folder)
+        if os.path.isdir(instance_path):
+            ret[instance_folder] = instance_path
+    return ret
 
 if __name__ == "__main__":
     base_path = get_user_input("Set base path", base_path)
@@ -78,32 +95,44 @@ if __name__ == "__main__":
             break
     download_latest_dll(asset_url, dll_file_path)
 
-    instance_dirs = list(get_instance_dirs(base_path))
-    logger.info(f'Found instances: {[os.path.basename(x) for x in instance_dirs]}')
-    for instance_folder in copy(instance_dirs):
-        instance_name = os.path.basename(instance_folder)
+    instance_dirs = get_instance_dirs(base_path)
+    logger.info(f'Found instances: {[name for name,path in instance_dirs.items()]}')
+    for instance_name, instance_folder in dict(instance_dirs).items():
         choice = get_user_input(f'Process instance {instance_name}?', 'y')
         if choice.lower() != 'y':
             logger.info(f'Skipping instance {instance_name}')
-            instance_dirs.remove(instance_folder)
-    logger.info(f'Selected instances: {[os.path.basename(x) for x in instance_dirs]}')
+            del instance_dirs[instance_name]
+    logger.info(f'Selected instances: {[name for name,path in instance_dirs.items()]}')
 
-    for instance_folder in instance_dirs:
-        instance_name = os.path.basename(instance_folder)
+    for instance_name, instance_folder in instance_dirs.items():
         logger.info(f'Stopping instance {instance_name}')
         execute_os_command(['ampinstmgr', 'stop', instance_name])
 
     execute_os_command(['ampinstmgr', 'reactivate', 'ADS01', developer_license_key])
     execute_os_command(['taskkill', '/f', '/im', 'AMP.exe'])
-    for instance_folder in instance_dirs:
-        instance_name = os.path.basename(instance_folder)
-        discordbot_plugin_dir = os.path.join(instance_folder, 'Plugins', 'DiscordBotPlugin')
+    for instance_name, instance_folder in instance_dirs.items():
         logger.info(f'Processing instance {instance_name}')
-        os.makedirs(discordbot_plugin_dir, exist_ok=True)
-        shutil.copy2(dll_file_path, discordbot_plugin_dir)
+        copy_dll_to_plugin_folder(instance_folder)
         update_amp_config(instance_folder)
 
-    for instance_folder in instance_dirs:
+    for instance_name, instance_folder in instance_dirs.items():
+        plugin_config_file = os.path.join(instance_folder, plugin_config_file_name)
+        if os.path.exists(plugin_config_file):
+            print(f'{plugin_config_file_name} exists in instance {instance_name}')
+            choice = get_user_input('Do you want to copy it to all other selected instances? (does not overwrite)', 'n')
+            if choice.lower() == 'y':
+                for other_instance_name, other_instance_folder in instance_dirs.items():
+                    if other_instance_name != instance_name:
+                        other_plugin_config_file = os.path.join(other_instance_folder, plugin_config_file_name)
+                        if os.path.exists(other_plugin_config_file):
+                            logger.info(f'{other_plugin_config_file} already exists, skipping')
+                        else:
+                            logger.info(f'Copying {plugin_config_file} to {other_plugin_config_file}')
+                            shutil.copy2(plugin_config_file, other_plugin_config_file)
+            break
+            
+
+    for instance_folder in instance_dirs.items():
         instance_name = os.path.basename(instance_folder)
         logger.info(f'Starting instance {instance_name}')
         execute_os_command(['ampinstmgr', 'start', instance_name])
